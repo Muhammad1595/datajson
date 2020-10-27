87b28ac4b9f914f10999b70431046cadbbe0c436
Muhammad1595 <71817155+Muhammad1595@users.noreply.github.com>
2020-10-27 20:29:43 +0400

    Create python.py

---
+++ python.py
@@ -0,0 +1,10 @@
+import discord
+from discord.ext import commands
+import os
+import random
+import json
+import requests
+from os import getcwd
+
+url = "https://github.com/someguy/brilliant/blob/master/somefile.txt"
+directory = getcwd()
